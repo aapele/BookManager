43b3427aad803945e81b0322b39527a880de8cc7
aapele <165931817+aapele@users.noreply.github.com>
2024-06-10 22:52:24 +0300

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,25 @@
+public class Book {
+  private String title;
+  private String author;
+  private int pages;
+
+  public Book(){}
+  public Book(String title, String author, int pages){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+  }
+
+  public String getTitle(){
+    return title;
+  }
+
+  public String getAuthor(){
+    return author;
+  }
+
+  public int getPages(){
+    return pages;
+  }
+  
+}
